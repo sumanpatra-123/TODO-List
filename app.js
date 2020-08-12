733ffd035a397287f7f4e581bdfa999f64db6c82
sumanpatra-123 <64701665+sumanpatra-123@users.noreply.github.com>
2020-08-12 14:15:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+//jshint esversion:6
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+console.log(date());
+
+const app = express();
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+
+  let day = date();
+
+  res.render("list", {listTitle: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res) {
+
+  let item = req.body.newItem;
+
+  if (req.body.list == "work") {
+    workItems.push(item);
+    res.redirect("/work")
+  } else {
+    items.push(item)
+    res.redirect("/");
+  }
+
+
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "work List",
+    newListItems: workItems
+  });
+})
+
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
+
+app.listen(3000, function() {
+  console.log("Sever started on port 3000");
+});
